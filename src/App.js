ee3bb016c90f231f9633f375cd9b21f7996500ec
Moezgarraoui <garraouimouez@gmail.com>
2022-10-31 21:12:31 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from 'react-bootstrap/Button';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Formm  from "./Formm.js";
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Formm/>
+    </div>
+  );
+}
+
+export default App;
